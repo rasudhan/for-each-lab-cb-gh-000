c8e7cf8dfd81571262d4ac9488c260c3aafe03dc
Anandha Sudhan <rasudhan@gmail.com>
2018-10-05 00:25:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function iterativeLog(array) {
-  array.forEach(()=> {
-    for(let i=0;i<array.length;i++) {
-      console.log(${i}: ${array[i]});
-    }
+  array.forEach((el, i) => {
+    console.log(`${i}: ${el}`)
   })
 }
