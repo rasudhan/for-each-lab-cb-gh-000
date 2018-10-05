3e99dd60766892cc7c15be0277dfb05d0fef6c3d
Anandha Sudhan <rasudhan@gmail.com>
2018-10-05 00:28:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  const colors=["red","blue","green"];
+
+  colors.forEach(callback);
+  return colors
+}
+
+function doToArray(array,callback) {
+  array.forEach(callback);
+
+}
